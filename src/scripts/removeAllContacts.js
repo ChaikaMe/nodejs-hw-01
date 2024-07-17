2f40ef7b7c9322af8f4faee92b4bb39cc5e1473a
ClownTripleSrank <145134985+ChaikaMe@users.noreply.github.com>
2024-07-17 22:19:46 +0300

    first-push

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,5 +1,17 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
 
-export const removeAllContacts = async () => {};
+export const removeAllContacts = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = JSON.parse(data);
+
+    contacts.length = 0;
+
+    await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 removeAllContacts();
--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,5 +1,19 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
 
-export const removeLastContact = async () => {};
+export const removeLastContact = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = JSON.parse(data);
+
+    if (contacts.length > 0) {
+      contacts.pop();
+    } else return;
+
+    await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 removeLastContact();
